bc4424fe5cf5f85e58d77c09f293da486872449a
3bdalla3adil <3bdalla995@gmail.com>
2022-10-24 09:30:43 +0300

    comment out unnecessary function

--- models/models.py
+++ models/models.py
@@ -14,28 +14,28 @@ class EmployeeMailComposer(models.TransientModel):
     _description = 'Employee EMail Wizard that allow to send an Email with an attachment'
 
     # employee_ids = fields.Many2many('hr.employee', string='Employees')
-    @api.model
-    def send_mail(self,auto_commit=False):
-        print("=====================self============================")
-        print(self)
+    # @api.model
+    # def send_mail(self,auto_commit=False):
+    #     print("=====================self============================")
+    #     print(self)
 
-        for res in self.recieptient_ids:
-            self.env['mail.message'].sudo().create({
-                'message_type': "notification",
-                'email_from': "Employee Email" ,  # ToDo:need to be changed to Employee Email
-                'body': "an invitation for attending this event",
-                'subject': "Invitation ",
-                'model': "hr.employee",
-                'res_id': res.id,
-                'partner_ids': [res.id],
-                'author_id': "HR Employee",  # ToDo:need to be changed to Email partner id
-                'date': False,
-                'notification_ids': [
-                    (0, 0, {'res_partner_id': res.id, 'notification_type': 'inbox'})
-                ]})
-        self.env.cr.commit()
+    #     for res in self.recieptient_ids:
+    #         self.env['mail.message'].sudo().create({
+    #             'message_type': "notification",
+    #             'email_from': "Employee Email" ,  # ToDo:need to be changed to Employee Email
+    #             'body': "an invitation for attending this event",
+    #             'subject': "Invitation ",
+    #             'model': "hr.employee",
+    #             'res_id': res.id,
+    #             'partner_ids': [res.id],
+    #             'author_id': "HR Employee",  # ToDo:need to be changed to Email partner id
+    #             'date': False,
+    #             'notification_ids': [
+    #                 (0, 0, {'res_partner_id': res.id, 'notification_type': 'inbox'})
+    #             ]})
+    #     self.env.cr.commit()
 
-        res = super(MailComposer,self).send_mail(auto_commit=auto_commit)
-        return res
+    #     res = super(MailComposer,self).send_mail(auto_commit=auto_commit)
+    #     return res
 
 
